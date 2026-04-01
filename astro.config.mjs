a3ba8eb57d3199291b3c3d1165d504e6d88c63ff
Sam O'Donnell <74621903+samuelod-atmos@users.noreply.github.com>
2026-04-01 14:09:43 -0600

    Configure for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://samuelod-atmos.github.io',
-  base: '/samuelod-atmos.github.io/',
   integrations: [react()]
 });
